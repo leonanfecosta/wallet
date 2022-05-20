d93983995499200ed2bff74f95d54735908c0bb1
Leonan Costa <leonanfecosta@gmail.com>
2022-05-20 17:05:18 -0300

    implementa a lógica para que a tabela seja alimentada pelo estado da aplicação

--- src/components/ExpensesTable.jsx
+++ src/components/ExpensesTable.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class ExpensesTable extends React.Component {
   render() {
@@ -14,16 +15,51 @@ class ExpensesTable extends React.Component {
       'Moeda de conversão',
       'Editar/Excluir',
     ];
+    const { expenses } = this.props;
     return (
       <table>
         <thead>
           <tr>
-            { titles.map((title, index) => <th key={ index }>{ title }</th>) }
+            {titles.map((title, index) => (
+              <th key={ index }>{title}</th>
+            ))}
           </tr>
         </thead>
+        <tbody>
+          {expenses.map((expense) => {
+            const coin = expense.exchangeRates[expense.currency];
+            const { name, ask } = coin;
+            const coinName = name.split('/')[0];
+            return (
+              <tr key={ expense.id }>
+                <td>{expense.description}</td>
+                <td>{expense.tag}</td>
+                <td>{expense.method}</td>
+                <td>{Number(expense.value).toFixed(2)}</td>
+                <td>{coinName}</td>
+                <td>{Number(ask).toFixed(2)}</td>
+                <td>{Number(expense.value * ask).toFixed(2)}</td>
+                <td>Real</td>
+                <td>
+                  <button type="button" id={ expense.id }>
+                    Excluir
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
       </table>
     );
   }
 }
 
-export default connect()(ExpensesTable);
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+ExpensesTable.propTypes = {
+  expenses: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default connect(mapStateToProps)(ExpensesTable);
