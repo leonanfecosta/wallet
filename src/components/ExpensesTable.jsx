9cc8a7fb8b1f9cb6c50ef2c96c7991a40f2e34a6
Leonan Costa <leonanfecosta@gmail.com>
2022-05-20 14:00:03 -0300

    corrige erro de lint

--- src/components/ExpensesTable.jsx
+++ src/components/ExpensesTable.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 class ExpensesTable extends React.Component {
   render() {
-    const { expenses } = this.props;
     const titles = [
       'Descrição',
       'Tag',
